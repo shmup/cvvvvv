0a2ae63a21764a7b2a48ca1c2ec15ea370b32ac1
Jared Miller <jared@smell.flowers>
2023-10-28 17:17:33 -0400

    Cleanup pandoc.sh with shfmt

--- pandoc.sh
+++ pandoc.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
+
 INFILE="resume.md"
 OUTFILE="resume.pdf"
 CSSFILE="github.css"
 
-run_pandoc () {
-    pandoc \
-        -f markdown \
-        -t html \
-        "$INFILE" \
-        -o "$OUTFILE" \
-        --metadata pagetitle="required_but_unused" \
-        --self-contained \
-        --css="$CSSFILE" \
-        --pdf-engine-opt=--enable-local-file-access
+generate_pdf() {
+  pandoc \
+    -f markdown \
+    -t html \
+    "$INFILE" \
+    -o "$OUTFILE" \
+    --metadata pagetitle="required_but_unused" \
+    --self-contained \
+    --css="$CSSFILE" \
+    --pdf-engine="wkhtmltopdf" \
+    --pdf-engine-opt=--enable-local-file-access
 }
 
-if [ -z $GLORT_WATCH ];
-    then
-        run_pandoc;
-    else
-        run_pandoc;
-        echo "Watching for changes..."
-        while inotifywait -qe modify resume.md github.css;
-        do run_pandoc;
-    done
+generate_pdf
+
+if [ -n "$GLORT_WATCH" ]; then
+  echo "Watching for changes..."
+  while inotifywait -qe modify resume.md github.css; do
+    generate_pdf
+  done
 fi
